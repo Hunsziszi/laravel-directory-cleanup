ba02b7f6014b0df3fa1be1076d2295940769ee33
Váczi Szilárd <vacziszil@gmail.com>
2021-04-04 11:43:49 +0200

    replace directory slashes to php directory separator update composer.json versions, test script change windows compatibile

--- src/DirectoryCleanupServiceProvider.php
+++ src/DirectoryCleanupServiceProvider.php
@@ -9,13 +9,23 @@ class DirectoryCleanupServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/laravel-directory-cleanup.php' => config_path('laravel-directory-cleanup.php'),
+            __DIR__
+            . DIRECTORY_SEPARATOR . '..'
+            . DIRECTORY_SEPARATOR
+            . 'config'
+            . DIRECTORY_SEPARATOR
+            . 'laravel-directory-cleanup.php' => config_path('laravel-directory-cleanup.php'),
         ], 'config');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/laravel-directory-cleanup.php', 'laravel-directory-cleanup');
+        $this->mergeConfigFrom(__DIR__
+            . DIRECTORY_SEPARATOR . '..'
+            . DIRECTORY_SEPARATOR
+            . 'config'
+            . DIRECTORY_SEPARATOR
+            . 'laravel-directory-cleanup.php', 'laravel-directory-cleanup');
 
         $this->app->bind('command.clean:directories', DirectoryCleanupCommand::class);
 
--- tests/DirectoryCleanupTest.php
+++ tests/DirectoryCleanupTest.php
@@ -21,8 +21,8 @@ class DirectoryCleanupTest extends TestCase
 
         foreach ($directories as $directory => $config) {
             foreach (range(1, $numberOfDirectories) as $ageInMinutes) {
-                $this->createFile("{$directory}/{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
-                $this->createFile("{$directory}/.{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
+                $this->createFile("{$directory}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
+                $this->createFile("{$directory}".DIRECTORY_SEPARATOR.".{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
             }
         }
 
@@ -31,11 +31,11 @@ class DirectoryCleanupTest extends TestCase
         foreach ($directories as $directory => $config) {
             foreach (range(1, $numberOfDirectories) as $ageInMinutes) {
                 if ($ageInMinutes < $config['deleteAllOlderThanMinutes']) {
-                    $this->assertFileExists("{$directory}/{$ageInMinutes}MinutesOld.txt");
-                    $this->assertFileExists("{$directory}/.{$ageInMinutes}MinutesOld.txt");
+                    $this->assertFileExists("{$directory}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
+                    $this->assertFileExists("{$directory}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
                 } else {
-                    $this->assertFileNotExists("{$directory}/{$ageInMinutes}MinutesOld.txt");
-                    $this->assertFileNotExists("{$directory}/.{$ageInMinutes}MinutesOld.txt");
+                    $this->assertFileDoesNotExist("{$directory}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
+                    $this->assertFileDoesNotExist("{$directory}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
                 }
             }
         }
@@ -48,36 +48,36 @@ class DirectoryCleanupTest extends TestCase
 
         $directories = [];
 
-        $this->getTempDirectory('top/'.implode('/', range(1, $numberSubOfDirectories)), true);
+        $this->getTempDirectory('top/'.implode(DIRECTORY_SEPARATOR, range(1, $numberSubOfDirectories)), true);
         $directories[$this->getTempDirectory('top')] = ['deleteAllOlderThanMinutes' => 3];
 
         $this->app['config']->set('laravel-directory-cleanup', compact('directories'));
 
-        $path = $this->getTempDirectory('top').'/';
+        $path = $this->getTempDirectory('top').DIRECTORY_SEPARATOR;
         foreach (range(1, $numberSubOfDirectories + 1) as $level) {
             foreach (range(1, $numberSubOfDirectories) as $ageInMinutes) {
-                $this->createFile("{$path}/{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
-                $this->createFile("{$path}/.{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
+                $this->createFile("{$path}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
+                $this->createFile("{$path}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt", $ageInMinutes);
             }
-            $path .= "{$level}/";
+            $path .= "{$level}".DIRECTORY_SEPARATOR;
         }
 
         $this->artisan('clean:directories');
 
         foreach ($directories as $directory => $config) {
-            $path = $directory.'/';
+            $path = $directory.DIRECTORY_SEPARATOR;
 
             foreach (range(1, $numberSubOfDirectories + 1) as $level) {
                 foreach (range(1, $numberSubOfDirectories) as $ageInMinutes) {
                     if ($ageInMinutes < $config['deleteAllOlderThanMinutes']) {
-                        $this->assertFileExists("{$path}/{$ageInMinutes}MinutesOld.txt");
-                        $this->assertFileExists("{$path}/.{$ageInMinutes}MinutesOld.txt");
+                        $this->assertFileExists("{$path}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
+                        $this->assertFileExists("{$path}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
                     } else {
-                        $this->assertFileNotExists("{$path}/{$ageInMinutes}MinutesOld.txt");
-                        $this->assertFileNotExists("{$path}/.{$ageInMinutes}MinutesOld.txt");
+                        $this->assertFileDoesNotExist("{$path}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
+                        $this->assertFileDoesNotExist("{$path}".DIRECTORY_SEPARATOR."{$ageInMinutes}MinutesOld.txt");
                     }
                 }
-                $path .= "{$level}/";
+                $path .= "{$level}".DIRECTORY_SEPARATOR;
             }
         }
     }
@@ -89,20 +89,20 @@ class DirectoryCleanupTest extends TestCase
             'deleteAllOlderThanMinutes' => 5,
         ];
 
-        $cleanup_policy = \Spatie\DirectoryCleanup\Test\CustomCleanupCleanupPolicy::class;
+        $cleanup_policy = CustomCleanupCleanupPolicy::class;
 
         $this->app['config']->set('laravel-directory-cleanup', compact('directories', 'cleanup_policy'));
 
         foreach ($directories as $directory => $config) {
-            $this->createFile("{$directory}/keepThisFile.txt", 5);
-            $this->createFile("{$directory}/removeThisFile.txt", 5);
+            $this->createFile("{$directory}".DIRECTORY_SEPARATOR."keepThisFile.txt", 5);
+            $this->createFile("{$directory}".DIRECTORY_SEPARATOR."removeThisFile.txt", 5);
         }
 
         $this->artisan('clean:directories');
 
         foreach ($directories as $directory => $config) {
-            $this->assertFileExists("{$directory}/keepThisFile.txt");
-            $this->assertFileNotExists("{$directory}/removeThisFile.txt");
+            $this->assertFileExists("{$directory}".DIRECTORY_SEPARATOR."keepThisFile.txt");
+            $this->assertFileDoesNotExist("{$directory}".DIRECTORY_SEPARATOR."/removeThisFile.txt");
         }
     }
 
@@ -118,13 +118,13 @@ class DirectoryCleanupTest extends TestCase
             'deleteAllOlderThanMinutes' => 3,
         ];
 
-        $this->createFile("{$existingDirectory}/5MinutesOld.txt", 5);
+        $this->createFile("{$existingDirectory}".DIRECTORY_SEPARATOR."5MinutesOld.txt", 5);
 
         $this->app['config']->set('laravel-directory-cleanup', compact('directories'));
 
         $this->artisan('clean:directories');
 
-        $this->assertFileNotExists("{$existingDirectory}/5MinutesOld.txt");
+        $this->assertFileDoesNotExist("{$existingDirectory}".DIRECTORY_SEPARATOR."5MinutesOld.txt");
     }
 
     /** @test */
@@ -138,23 +138,23 @@ class DirectoryCleanupTest extends TestCase
         $this->app['config']->set('laravel-directory-cleanup', compact('directories'));
 
         foreach ($directories as $directory => $config) {
-            $this->createDirectory("{$directory}/emptyDir");
-            $this->createFile("{$directory}/emptyDir/5MinutesOld.txt", 5);
-            $this->createDirectory("{$directory}/notEmptyDir");
-            $this->createFile("{$directory}/notEmptyDir/1MinutesOld.txt", 1);
-            $this->createDirectory("{$directory}/emptyDirWithHiddenFile");
-            $this->createFile("{$directory}/emptyDirWithHiddenFile/.5MinutesOld.txt", 5);
-            $this->createDirectory("{$directory}/notEmptyDirWithHiddenFile");
-            $this->createFile("{$directory}/notEmptyDirWithHiddenFile/.1MinutesOld.txt", 1);
+            $this->createDirectory("{$directory}".DIRECTORY_SEPARATOR."emptyDir");
+            $this->createFile("{$directory}".DIRECTORY_SEPARATOR."emptyDir/5MinutesOld.txt", 5);
+            $this->createDirectory("{$directory}".DIRECTORY_SEPARATOR."notEmptyDir");
+            $this->createFile("{$directory}".DIRECTORY_SEPARATOR."notEmptyDir/1MinutesOld.txt", 1);
+            $this->createDirectory("{$directory}".DIRECTORY_SEPARATOR."emptyDirWithHiddenFile");
+            $this->createFile("{$directory}".DIRECTORY_SEPARATOR."emptyDirWithHiddenFile/.5MinutesOld.txt", 5);
+            $this->createDirectory("{$directory}".DIRECTORY_SEPARATOR."notEmptyDirWithHiddenFile");
+            $this->createFile("{$directory}".DIRECTORY_SEPARATOR."notEmptyDirWithHiddenFile/.1MinutesOld.txt", 1);
         }
 
         $this->artisan('clean:directories');
 
         foreach ($directories as $directory => $config) {
-            $this->assertDirectoryExists("{$directory}/notEmptyDir");
-            $this->assertDirectoryNotExists("{$directory}/emptyDir");
-            $this->assertDirectoryExists("{$directory}/notEmptyDirWithHiddenFile");
-            $this->assertDirectoryNotExists("{$directory}/emptyDirWithHiddenFile");
+            $this->assertDirectoryExists("{$directory}".DIRECTORY_SEPARATOR."notEmptyDir");
+            $this->assertDirectoryDoesNotExist("{$directory}".DIRECTORY_SEPARATOR."emptyDir");
+            $this->assertDirectoryExists("{$directory}".DIRECTORY_SEPARATOR."notEmptyDirWithHiddenFile");
+            $this->assertDirectoryDoesNotExist("{$directory}".DIRECTORY_SEPARATOR."emptyDirWithHiddenFile");
         }
     }
 
